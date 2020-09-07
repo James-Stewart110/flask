8ef1f770ac88bfcfe572e70699a189c8f9122fa1
James-Stewart110 <jamesstewart506@hotmail.co.uk>
2020-09-07 11:55:52 +0000

    flask: POST

--- run.py
+++ run.py
@@ -27,8 +27,10 @@ def about_member(member_name):
 
     return render_template('member.html', member=member)
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        print(request.form)
     return render_template('contact.html', page_title='Contact')
 
 @app.route('/careers')
